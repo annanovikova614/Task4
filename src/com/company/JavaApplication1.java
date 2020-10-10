070d5e06b97355f4237ea74a3e044bd2d757c571
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-10 22:15:43 +0300

    Update JavaApplication1.java

--- src/com/company/JavaApplication1.java
+++ src/com/company/JavaApplication1.java
@@ -2,25 +2,24 @@ package com.company;
 import java. util.Scanner;
 
  class Main {
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         printCalc();
     }
-    public static void printCalc(){
+    public static void printCalc() {
         System.out.println(calc1());
         System.out.println(calc2());
-        System.out.println(); //Выводим на экран перевод строк
+        System.out.println(); 
     }
     public static double calc1() {
         double sum = 0;
-        for (int i = 1; i < 51; i++)//перебераем числа от 1 до 50
-            sum += 1.0 / (i * i * i);//добавляем в сумму слагаемое 1, 1/2^3, 1/3^3 ...
+        for (int i = 1; i < 51; i++)
+            sum += 1.0 / (i * i * i);
         return sum;
     }
     public static double calc2() {
         double sum = 0;
-        for (int i = 2; i < 129; i += 2)//перебераем числа от 2 до 128 с шагом 2
-            sum += 1.0 / (i * i);//добавляем в сумму слагаемое 1/2^2, 1/4^2, 1/6^2 ...
+        for (int i = 2; i < 129; i += 2)
+            sum += 1.0 / (i * i);
         return sum;
     }
 }
