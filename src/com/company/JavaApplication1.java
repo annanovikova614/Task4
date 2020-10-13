19bead85d20dd60ed057e0d882e99202c1451431
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-13 18:51:46 +0300

    Update JavaApplication1.java

--- src/com/company/JavaApplication1.java
+++ src/com/company/JavaApplication1.java
@@ -1,25 +1,28 @@
 package com.company;
-import java. util.Scanner;
 
- class Main {
-     public static void main(String[] args) {
-        printCalc();
+public class Main {
+        public static void main(String[] args) {
+            printTheResultOfCalculation();
+        }
+        public static void printTheResultOfCalculation() {
+            System.out.println(calculateTheFirstResult());
+            System.out.println(calculateTheSecondResult());
+            System.out.println();
+        }
+        public static double calculateTheFirstResult() {
+            double sum = 0;
+            for (int i = 1; i < 51; i++)
+                sum += 1.0 / (i * i * i);
+                {
+                    return sum;
+                }
+        }
+        public static double calculateTheSecondResult() {
+            double sum = 0;
+            for (int i = 2; i < 129; i += 2)
+                sum += 1.0 / (i * i);
+                {
+                    return sum;
+                }
+        }
     }
-    public static void printCalc() {
-        System.out.println(calc1());
-        System.out.println(calc2());
-        System.out.println(); 
-    }
-    public static double calc1() {
-        double sum = 0;
-        for (int i = 1; i < 51; i++)
-            sum += 1.0 / (i * i * i);
-        return sum;
-    }
-    public static double calc2() {
-        double sum = 0;
-        for (int i = 2; i < 129; i += 2)
-            sum += 1.0 / (i * i);
-        return sum;
-    }
-}
