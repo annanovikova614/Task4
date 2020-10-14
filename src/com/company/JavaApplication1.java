51300e9985f7ca1d5cc0eed8b3882933cdb8d422
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-14 20:24:48 +0300

    Update JavaApplication1.java

--- src/com/company/JavaApplication1.java
+++ src/com/company/JavaApplication1.java
@@ -1,29 +1,35 @@
 package com.company;
 
 public class Main {
-        public static void main(String[] args) 
+        public static void main(String[] args)
         {
             printTheResultOfCalculation();
         }
+
         public static void printTheResultOfCalculation() {
-            System.out.println(calculateTheFirstResult());
-            System.out.println(calculateTheSecondResult());
+            double sum1 = calculateTheFirstResult();
+            double sum2 = calculateTheSecondResult();
+            System.out.println("Результат первого вычисления = " + sum1);
+            System.out.println("Результат второго вычисления = " + sum2);
             System.out.println();
         }
+
         public static double calculateTheFirstResult() {
-            double sum = 0;
+            double sum1 = 0;
             for (int i = 1; i < 51; i++)
-                sum += 1.0 / (i * i * i);
+                sum1 += 1.0 / (i * i * i);
                 {
-                    return sum;
+                    return sum1;
                 }
         }
+
         public static double calculateTheSecondResult() {
-            double sum = 0;
+            double sum2 = 0;
             for (int i = 2; i < 129; i += 2)
-                sum += 1.0 / (i * i);
+                sum2 += 1.0 / (i * i);
                 {
-                    return sum;
+                    return sum2;
                 }
         }
     }
+
