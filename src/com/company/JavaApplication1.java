e5c979e72a79d568c3338c227be0509113d08842
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-21 08:14:47 +0300

    Update JavaApplication1.java

--- src/com/company/JavaApplication1.java
+++ src/com/company/JavaApplication1.java
@@ -18,21 +18,23 @@ public class Main {
     public static double calculateTheFirstResult()
     {
         double sum1 = 0;
-        for (int i = 1; i < 51; i++)
+        for (int i = 1; i < 51; i++) {
             sum1 += 1.0 / (i * i * i);
         {
             return sum1;
         }
+      }
     }
 
     public static double calculateTheSecondResult() 
     {
         double sum2 = 0;
-        for (int i = 2; i < 129; i += 2)
+        for (int i = 2; i < 129; i += 2) {
             sum2 += 1.0 / (i * i);
         {
             return sum2;
         }
+      }
     }
 }
 
