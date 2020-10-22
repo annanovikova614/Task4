9cd0476024179d1a6460188fc1b37138200428e2
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-22 21:18:28 +0300

    Update JavaApplication1.java

--- src/com/company/JavaApplication1.java
+++ src/com/company/JavaApplication1.java
@@ -1,41 +1,34 @@
 package com.company;
 
 public class Main {
-        
-    public static void main(String[] args)
-    {
-        printTheResultOfCalculation();
-    }
 
-    public static void printTheResultOfCalculation() {
+    public static void main(String[] args) {
         double sum1 = calculateTheFirstResult();
         double sum2 = calculateTheSecondResult();
-        System.out.println("Результат первого вычисления = " + sum1);
-        System.out.println("Результат второго вычисления = " + sum2);
+        calculateTheFirstResult();
+        calculateTheSecondResult();
+        printResultsOfCalculation(sum1,sum2);
+    }
+
+    private static void printResultsOfCalculation(double sum1, double sum2) {
+        System.out.printf("Результат первого вычисления %.3f", sum1);
         System.out.println();
+        System.out.printf("Результат второго вычисления %.3f", sum2);
     }
 
-    public static double calculateTheFirstResult()
-    {
+    public static double calculateTheFirstResult() {
         double sum1 = 0;
-        for (int i = 1; i < 51; i++) {
-            sum1 += 1.0 / (i * i * i);
-        {
-            return sum1;
-        }
-      }
+            for (int i = 1; i < 51; i++) {
+                sum1 += 1.0 / (i * i * i);
+            }
+        return sum1;
     }
 
-    public static double calculateTheSecondResult() 
-    {
+    public static double calculateTheSecondResult() {
         double sum2 = 0;
-        for (int i = 2; i < 129; i += 2) {
-            sum2 += 1.0 / (i * i);
-        {
-            return sum2;
-        }
-      }
+            for (int i = 2; i < 129; i += 2) {
+                sum2 += 1.0 / (i * i);
+            }
+        return sum2;
     }
 }
-
-
